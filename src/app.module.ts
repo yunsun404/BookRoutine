42094d765d9af665533b4b3f4f09c943404e5d01
YJY932 <jyjjang0802@gmail.com>
2026-05-29 00:24:36 +0900

    wip: backup

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,18 @@ import { ConfigModule } from '@nestjs/config';  // ← 추가
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FirebaseService } from './firebase/firebase.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { ReadingRoomModule } from './reading-room/reading-room.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),  // ← 추가
+    ConfigModule.forRoot({ isGlobal: true }),  // ← added
+    AuthModule,
+    UserModule,
+    ReadingRoomModule
   ],
   controllers: [AppController],
   providers: [AppService, FirebaseService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,9 +1,9 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
-@Global() // 전역으로 사용 가능하게
+@Global()
 @Module({
   providers: [PrismaService],
   exports: [PrismaService],
 })
-export class PrismaModule {}
+export class PrismaModule { }
